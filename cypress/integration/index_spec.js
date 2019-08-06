ca5d7ab591c9c3aa70ae29d71360b4253bff48be
Samuel Otterman <sjotterman@gmail.com>
2019-08-06 19:27:34 -0400

    fix css names for passing cypress test

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,5 +1,5 @@
-describe("Basic functionality", function() {
-  it("Adds an item", function() {
+describe("Basic functionality", function () {
+  it("Adds an item", function () {
     cy.visit("/");
     cy.get(".App")
       .contains("Waffle House")
@@ -10,10 +10,10 @@ describe("Basic functionality", function() {
     cy.get('.App input[type="submit"').click();
     cy.get(".App #itemInput").type("On Tap");
     cy.get('.App input[type="submit"').click();
-    cy.get(".ListItem").contains("Waffle House");
-    cy.get(".ListItem").contains("Burger King");
-    cy.get(".ListItem").contains("On Tap");
-    cy.get("#item_1 .CloseItem").click();
+    cy.get(".listItem").contains("Waffle House");
+    cy.get(".listItem").contains("Burger King");
+    cy.get(".listItem").contains("On Tap");
+    cy.get("#item_1 .closeItem").click();
     cy.get(".App")
       .contains("Burger King")
       .should("not.exist");
